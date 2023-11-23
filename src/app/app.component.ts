a411df9728141db21e48e63bd3ea57ea5716949f
Matheo <matheo.gaillac@lmdsio.fr>
2023-11-23 15:20:24 +0100

    Feat: Add Compteur Calcul

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {CalculetteComponent} from "./calculette/calculette.component";
+import {CompteurDeCalculComponent} from "./compteur-de-calcul/compteur-de-calcul.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CalculetteComponent],
+    imports: [CommonModule, RouterOutlet, CalculetteComponent, CompteurDeCalculComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/calculette/calculette.component.ts
+++ src/app/calculette/calculette.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule, FormControl, FormGroup, Validators} from "@angular/forms";
+import {CompteurDeCalculComponent} from "../compteur-de-calcul/compteur-de-calcul.component";
 
 @Component({
   selector: 'app-calculette',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, CompteurDeCalculComponent],
   templateUrl: './calculette.component.html',
   styleUrl: './calculette.component.css'
 })
@@ -13,12 +14,18 @@ export class CalculetteComponent {
   public resultat: Number = 0;
   public calculatrice: FormGroup;
 
+  @ViewChild('compteur') compteur!: CompteurDeCalculComponent;
+
+  public flagTropDeCalcul: boolean = false;
+
   calcul(): void {
+    this.compteur.compte();
     this.resultat = eval(
       'parseFloat(this.operande1.value) ' +
       this.operateur.value +
       ' parseFloat(this.operande2.value)'
-    )};
+    )
+  };
 
 
   public operande1: FormControl
@@ -28,9 +35,9 @@ export class CalculetteComponent {
   public operateur: FormControl
     = new FormControl<string | null>('+', Validators.required);
   public operande2: FormControl
-    = new FormControl<Number>(0,[
-      Validators.min(1),
-      Validators.required
+    = new FormControl<Number>(0, [
+    Validators.min(1),
+    Validators.required
   ]);
 
   constructor() {
@@ -40,4 +47,11 @@ export class CalculetteComponent {
       operande2: this.operande2,
     });
   }
+
+  tropDeCalcul($event: boolean) {
+    if ($event == true) {
+      this.flagTropDeCalcul = true;
+    }
+  }
+
 }
